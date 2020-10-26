445962400a4daa8097d82b56ea766c7323d2edd5
Saeedeh M <saeedehm28@gmail.com>
2020-10-26 15:21:07 +0330

    changed utput to csv

--- network.py
+++ network.py
@@ -3,18 +3,18 @@ import networkx as nx
 import sys
 from sqlalchemy.orm.exc import NoResultFound
 import sqlite3 as sql
-
+import csv
 
 print('getting fiendship network for: ', sys.argv[1])
 
 
 def get_friendship_graph(sqllite_db):
     session_pg = pCur()
-    conn = sql.connect("sqlite/"+str(sqllite_db)+".db")
+    conn = sql.connect("sqlite/" + str(sqllite_db) + ".db")
     cursor = conn.cursor()
     cursor.execute("select userid from dbUser")
     users_list = cursor
-    #conn.close()
+    # conn.close()
     edge_list = []
     dlist = []
     for user in users_list:
@@ -35,16 +35,20 @@ def get_friendship_graph(sqllite_db):
             for flw in page.gfwlist():
                 edge = (flw, user[0])
                 edge_list.append(edge)
-        
-        break 
+
+        break
     print(edge_list)
+    with open("sqlite/" + str(sqllite_db) + ".csv", 'wb') as out:
+        csv_out = csv.writer(out, delimiter=";")
+        for row in edge_list:
+            csv_out.writerow(row)
     conn.close()
     session_pg.close()
-    #graph = nx.DiGraph()
-    g=nx.from_edgelist(edge_list)
-    nx.write_graphml(g, "sqlite/"+str(sqllite_db)+".graphml")
-    #return graph
+    # graph = nx.DiGraph()
+    # g=nx.from_edgelist(edge_list)
+    # nx.write_graphml(g, "sqlite/"+str(sqllite_db)+".graphml")
+    # return graph
+
 
 get_friendship_graph(sys.argv[1])
-print('graphml saved: ', sys.argv[1])
-
+print('csv saved: ', sys.argv[1])
