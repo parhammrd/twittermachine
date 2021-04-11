31b932321915b516302d22d4f68792910474e809
saeedehM <saeedehm28@gmail.com>
2021-04-11 16:56:12 +0430

    Update handleNeo4j.py

--- handleNeo4j.py
+++ handleNeo4j.py
@@ -21,12 +21,34 @@ class App:
                     p1=record['p1'], p2=record['p2']))
     def create_retweet(self, userId1, userId2, hashtag):
         with self.driver.session() as session:
+            if userId2 !=null:
             # Write transactions allow the driver to handle retries and transient errors
-            result = session.write_transaction(
-                self._create_and_return_retweet, userId1, userId2,hashtag)
+                result = session.write_transaction(
+                    self._create_and_return_retweet, userId1, userId2,hashtag)
+            else:
+                result = session.write_transaction(
+                    self._create_and_return_tweet, userId1, hashtag)
+
             for record in result:
                 print("Created retweet between: {p1}, {p2}".format(
                     p1=record['p1'], p2=record['p2']))
+
+    @staticmethod
+    def _create_and_return_tweet(tx, userId1, hashtag):
+        query = (
+            "MERGE (p1:Person {name: $userId1}) ON MATCH SET  p1.hashtag = p1.hashtag + $hashtag, p1.lastLoggedInAt = timestamp() ON CREATE SET  p1.hashtag= [$hashtag], p1.createdAt= timestamp()"
+            "RETURN p1"
+        )
+
+        result = tx.run(query, userId1=userId1, userId2=userId2, hashtag=hashtag)
+        try:
+            return [{"p1": record["p1"]["name"], "p2": record["p2"]["name"]}
+                    for record in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
     @staticmethod
     def _create_and_return_retweet(tx, userId1, userId2,hashtag):
         query=(
@@ -73,7 +95,7 @@ def connectToNeo4j():
     return app
     #app.close()
 
-def getfrnlayersnetwork(user):
+def getFrnNetwork(user):
     app=connectToNeo4j()
     frnpages = user.gfrpages()
     flwpages = user.gfwpages()
@@ -84,7 +106,7 @@ def getfrnlayersnetwork(user):
             for flw in page.gfwlist():
                 app.create_friendship(flw, user[0])
     app.close()
-def getRetweetLayersNetwork(userId,hashtag,retweetedFromId):
+def getRetweetNetwork(userId,hashtag,retweetedFromId):
     app=connectToNeo4j()
     app.create_retweet(userId,retweetedFromId,hashtag)
     app.close()
